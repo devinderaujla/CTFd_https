62ce1dd4ae7c616d99175f05c6d8424a8065df55
devinderaujla <79028432+devinderaujla@users.noreply.github.com>
2024-06-18 15:28:49 -0400

    Update installdependencies.sh

--- installdependencies.sh
+++ installdependencies.sh
@@ -5,6 +5,6 @@ curl -L https://github.com/docker/compose/releases/download/1.11.1/docker-compos
 chmod +x /usr/local/bin/docker-compose
 sudo openssl dhparam -out ./conf/nginx/cert/dhparam.pem 2048
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./conf/nginx/cert/nginx-selfsigned.key -out ./conf/nginx/cert/nginx-selfsigned.crt
-printf "\n If you continue then this script will launch docker-compose up command. If you dont wish to continue, press CTRL+C \n"
-pause
+#printf "\n If you continue then this script will launch docker-compose up command. If you dont wish to continue, press CTRL+C \n"
+read -n1 -r -p "\n If you continue then this script will launch docker-compose up command. If you dont wish to continue, press CTRL+C\n\nPress any key to continue..." key
 sudo docker-compose up
