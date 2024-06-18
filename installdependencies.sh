0ead4d9f1af96eea39e9f4b1fc38aa8fb1189801
devinderaujla <79028432+devinderaujla@users.noreply.github.com>
2024-06-18 15:01:04 -0400

    Create installdependencies.sh

---
+++ installdependencies.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+apt update
+apt install docker curl wget git -y
+curl -L https://github.com/docker/compose/releases/download/1.11.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
